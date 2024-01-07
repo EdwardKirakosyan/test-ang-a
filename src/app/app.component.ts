89b1e9e3450fef00adacdbc31a434a382054497e
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-07 14:11:50 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: ` title `,
+  imports: [CommonModule, RouterOutlet, TodoComponent],
+  template: `
+    <h1>Title</h1>
+    <app-todo />
+  `,
 })
 export class AppComponent {}
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [],
+  template: `
+    <h2>(TODO) Read Angular Essentials {{ taskTitle }} Guide</h2>
+    @if (isAdmin) {
+    <button>era</button>
+    } @else {
+    <p>You are not authorized.</p>
+    }
+
+    <p>{{ announcement }}</p>
+
+    <button (click)="save()">Save</button>
+
+    <ul>
+      @for (ingredient of ingredientList; track ingredient.name ) {
+      <li>{{ ingredient.name }}</li>
+      }
+    </ul>
+  `,
+  styles: `
+    li {
+      color: red;
+    }
+  `,
+})
+export class TodoComponent {
+  announcement = 'Hello again Angular!';
+
+  save() {
+    this.announcement = this.announcement.toUpperCase();
+  }
+
+  ingredientList = [
+    { name: 'noodles', quantity: 1 },
+    { name: 'miso broth', quantity: 1 },
+    { name: 'egg', quantity: 2 },
+  ];
+
+  isAdmin = true;
+  taskTitle = '';
+  isComplete = false;
+
+  completeTask() {
+    this.isComplete = true;
+  }
+  updateTitle(newTitle: string) {
+    this.taskTitle = newTitle;
+  }
+}
